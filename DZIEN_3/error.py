014bdad5ccee4ab72d9ecda494a24ffe5f1745d6
albim72 <marcin.albiniak72@gmail.com>
2022-09-21 15:08:01 +0200

    Update error.py

--- DZIEN_3/error.py
+++ DZIEN_3/error.py
@@ -3,11 +3,18 @@ try:
 except NameError:
     print("x nie istnieje!")
     x = float(input("podaj w takim razie x:"))
-    print(f"x wynosi: {x}")
+    if x == 0:
+        wynik = "....."
+    else:
+        wynik = x*2
 except:
     print("nieokreślony błąd")
 finally:
+    print(f"wynik x = {wynik}")
     y = float(input("podaj teraz drugą wartość y:"))
     print(f"y wynosi: {y}")
 
 print("ciąg dalszy...")
+
+print(f"wynik x = {wynik}")
+print(y)
