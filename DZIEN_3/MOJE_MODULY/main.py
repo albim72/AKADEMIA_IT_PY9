8b158d071a21720e0d96086d9d8eca7fec4f103d
albim72 <marcin.albiniak72@gmail.com>
2022-09-21 14:49:33 +0200

    Update main.py

--- DZIEN_3/MOJE_MODULY/main.py
+++ DZIEN_3/MOJE_MODULY/main.py
@@ -2,7 +2,19 @@
 #import dane as dn
 from dane import osoba
 from moje_funkcje import konferencja,policz
+#from miasto import Miasto
+from stolica import Stolica,Miasto
+from ekstra.fx.policzmy import licz
 
 print(osoba)
 print(konferencja("Jan Kowal"))
 print(f"wynik d = {policz(3,7)}")
+
+ms = Miasto(2,"Kielce","świętokrzyskie")
+ms.print_miasto()
+
+st = Stolica(1,"Warszawa","mazowieckie","Jan Kot")
+st.print_miasto()
+st.prezydent_info()
+
+print(f"wynik funkcji: {licz(45,7)}")
