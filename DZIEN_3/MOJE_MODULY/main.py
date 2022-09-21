e5c75024ac8d11a17b8d01e902100ed24966fc76
albim72 <marcin.albiniak72@gmail.com>
2022-09-21 14:20:17 +0200

    Update main.py

--- DZIEN_3/MOJE_MODULY/main.py
+++ DZIEN_3/MOJE_MODULY/main.py
@@ -1,1 +1,8 @@
+#import dane
+#import dane as dn
+from dane import osoba
+from moje_funkcje import konferencja,policz
 
+print(osoba)
+print(konferencja("Jan Kowal"))
+print(f"wynik d = {policz(3,7)}")
