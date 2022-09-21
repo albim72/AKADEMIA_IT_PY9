f2a615bae35d2be4245e771d7474868b8521b481
albim72 <marcin.albiniak72@gmail.com>
2022-09-21 15:45:47 +0200

    Update silnia.py

--- DZIEN_3/silnia.py
+++ DZIEN_3/silnia.py
@@ -1,7 +1,7 @@
 #n! = 1*2*..*n gdzie n -> liczba naturalna
 #double 1.8E+308
 #n????? double -> 171!
-
+import sys
 def silnia(n):
     if n<0:
         raise ValueError("silnia nie jest zdefiniowana dla liczb ujemnych")
@@ -10,8 +10,18 @@ def silnia(n):
         wynik *= i
     return wynik
 
+def silnia_rek(n):
+    if n<0:
+        raise ValueError("silnia nie jest zdefiniowana dla liczb ujemnych")
+    if n==0:
+        return 1
+    else:
+        return n*silnia_rek(n-1)
+
 try:
     n = int(input("podaj argument n silnii: "))
+    sys.setrecursionlimit(10000)
     print(f"silnia dla n = {n} wynosi: {silnia(n)}")
+    print(f"silnia rekurencyjna dla n = {n} wynosi: {silnia_rek(n)}")
 except ValueError as d:
     print(d)
